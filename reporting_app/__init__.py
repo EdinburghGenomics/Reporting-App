4ba8c1847b1a8af2fd68861901e3b0284305b19e
Murray Wham <murray.wham@ed.ac.uk>
2015-11-06 13:50:11 +0000

    Adding requirements.txt, configuring ports via args

--- reporting_app/__init__.py
+++ reporting_app/__init__.py
@@ -1,14 +1,12 @@
 __author__ = 'mwham'
+import argparse
 import tornado.wsgi
 import tornado.httpserver
 import tornado.ioloop
 import flask as fl
-import requests
+
 
 DEBUG = False
-base_url = 'http://127.0.0.1:5002/'
-eve_url = 'http://127.0.0.1:5002/'
-
 app = fl.Flask(__name__)
 app.config.from_object(__name__)
 
@@ -20,14 +18,13 @@ def main_page():
 
 @app.route('/project/<project>/')
 def report_project(project):
-    data = _fetch_data_points(sample_project=project)
-    return fl.render_template('project_overview.html', project=project, num_points=data['_meta']['total'])
+    return fl.render_template('project_overview.html', project=project, num_points=0)
 
 
 @app.route('/run_id/<run_id>/')
 def report_run(run_id):
     # data = _fetch_data_points(run_id=run_id)
-    return fl.render_template('run_overview.html', run_id=run_id, num_points=0)# data['_meta']['total'])
+    return fl.render_template('run_overview.html', run_id=run_id, num_points=0)  # data['_meta']['total'])
 
 
 @app.route('/<dataset_type>/<dataset>/demultiplexing')
@@ -35,25 +32,16 @@ def project_demultiplexing(dataset_type, dataset):
     return fl.render_template('demultiplexing_report.html', dataset_type=dataset_type, dataset=dataset)
 
 
-def _fetch_data_points(**queries):
-    query = _join(
-        eve_url,
-        'api/0.1/',
-        'data_points?where=',
-        '&'.join(('{type}=="{value}"'.format(type=k, value=v) for k, v in queries.items()))
-    )
-    print(query)
-    return requests.get(query).json()
-
-
 def _join(*parts):
     return ''.join(parts)
 
 
 if __name__ == '__main__':
-    print('setting up http server')
+
+    p = argparse.ArgumentParser()
+    p.add_argument('-p', '--port', type=int, help='port to run the Flask app on')
+    args = p.parse_args()
+
     http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
-    http_server.listen(5000)
-    print('starting')
+    http_server.listen(args.port)
     tornado.ioloop.IOLoop.instance().start()
- 
