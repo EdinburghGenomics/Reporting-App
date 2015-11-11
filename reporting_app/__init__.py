74942c914bdcabaf2312252a9f4c630f67ab99cf
Murray Wham <murray.wham@ed.ac.uk>
2015-11-11 15:41:29 +0000

    Adding datatable sorting, splitting off into reusable Jinja2 macro. Adding project/run/sample overviews. Making Tornado server optional.

--- reporting_app/__init__.py
+++ reporting_app/__init__.py
@@ -1,8 +1,5 @@
 __author__ = 'mwham'
 import argparse
-import tornado.wsgi
-import tornado.httpserver
-import tornado.ioloop
 import flask as fl
 import pymongo
 
@@ -13,6 +10,10 @@ app = fl.Flask(__name__)
 app.config.from_object(__name__)
 cli = pymongo.MongoClient('localhost', 4998)
 rest_api_base = 'http://localhost:4999/api/0.1'
+
+
+def rest_url(extension):
+    return rest_api_base + '/' + extension
 
 
 @app.route('/')
@@ -33,17 +34,84 @@ def run_reports():
 
 @app.route('/reports/runs/<run_id>')
 def report_run(run_id):
-    return fl.render_template('run_report.html', run_id=run_id)
+    return fl.render_template(
+        'run_report.html',
+        run_id=run_id,
+
+        datatables=[
+            {
+                'title': 'Demultiplexing',
+                'name': 'demultiplexing',
+                'api_url': rest_url('run_elements?where={"run_id":"%s"}' % run_id),
+                'cols': (
+                    ('lane', 'Lane'),
+                    ('barcode', 'Barcode'),
+                    ('project', 'Project'),
+                    ('library_id', 'Library ID'),
+                    ('sample_id', 'Sample ID'),
+                    ('pc_pass_filter', '% Pass-filter'),
+                    ('passing_filter_reads', 'Passing-filter reads'),
+                    ('yield_in_gb', 'Yield (Gb)'),
+                    ('pc_q30_r1', '% Q30 R1'),
+                    ('pc_q30_r2', '% Q30 R2')
+                )
+            },
+            {
+                'title': 'Unexpected Barcodes',
+                'name': 'unexpected_barcodes',
+                'api_url': rest_url('unexpected_barcodes?where={"run_id":"%s"}' % run_id),
+                'cols': (
+                    ('lane', 'Lane'),
+                    ('barcode', 'Barcode'),
+                    ('passing_filter_reads', 'Passing-filter reads'),
+                    ('pc_reads_in_lane', '% Reads in lane')
+                )
+            }
+        ]
+    )
 
 
-@app.route('/reports/samples/')
-def sample_reports():
-    return fl.render_template('samples.html')
+@app.route('/reports/projects/')
+def project_reports():
+    projects = set(x['project'] for x in cli['test_db']['samples'].find(projection={'project': True}))
+    return fl.render_template('projects.html', projects=projects)
 
 
-@app.route('/reports/samples/<sample_id>')
-def project_demultiplexing(sample_id):
-    return fl.render_template('sample_report.html', sample_id=sample_id)
+@app.route('/reports/projects/<project>')
+def report_project(project):
+    samples = set(x['sample_id'] for x in cli['test_db']['samples'].find(filter={'project': project}, projection={'sample_id': True}))
+    print(samples)
+
+    return fl.render_template(
+        'project_report.html',
+
+        project=project,
+        datatables=[
+            {
+                'title': project,
+                'name': project,
+                'api_url': rest_url('samples?where={"project":"%s"}' % project),
+                'cols': (
+                    ('sample_id', 'Sample ID'),
+                    ('library_id', 'Library ID'),
+                    ('user_sample_id', 'User sample ID'),
+                    ('yield_in_gb', 'Yield (Gb)'),
+                    ('initial_reads', 'Initial reads'),
+                    ('passing_filter_reads', 'Passing-filter reads'),
+                    ('nb_mapped_reads', '# mapped reads'),
+                    ('pc_mapped_reads', '% mapped reads'),
+                    ('nb_properly_mapped_reads', '# properly mapped reads'),
+                    ('pc_properly_mapped_reads', '% properly mapped reads'),
+                    ('nb_duplicate_reads', '# duplicate reads'),
+                    ('pc_duplicate_reads', '% duplicate reads'),
+                    ('median_coverage', 'Median coverage'),
+                    ('pc_callable', '% callable'),
+                    ('pc_q30_r1', '% Q30 R1'),
+                    ('pc_q30_r2', '% Q30 R2')
+                )
+            }
+        ]
+    )
 
 
 def _join(*parts):
@@ -54,8 +122,23 @@ if __name__ == '__main__':
 
     p = argparse.ArgumentParser()
     p.add_argument('-p', '--port', type=int, help='port to run the Flask app on')
+    p.add_argument('-d', '--debug', action='store_true', help='run the app in debug mode')
+    p.add_argument(
+        '-t',
+        '--tornado',
+        action='store_true',
+        help='whether to use Tornado\'s http server and wsgi container for external access'
+    )
     args = p.parse_args()
 
-    http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
-    http_server.listen(args.port)
-    tornado.ioloop.IOLoop.instance().start()
+    if args.tornado:
+        import tornado.wsgi
+        import tornado.httpserver
+        import tornado.ioloop
+
+        http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
+        http_server.listen(args.port)
+        tornado.ioloop.IOLoop.instance().start()
+
+    else:
+        app.run('localhost', 5000, debug=True)
--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -1,14 +1,18 @@
 __author__ = 'mwham'
 import argparse
-import tornado.wsgi
-import tornado.httpserver
-import tornado.ioloop
 import eve
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('-p', '--port', type=int, help='port to run Eve API on')
+    p.add_argument('-d', '--debug', action='store_true', help='run the app in debug mode')
     p.add_argument('-m', '--mongodb-port', type=int, help='port that mongoDB server is running on')
+    p.add_argument(
+        '-t',
+        '--tornado',
+        action='store_true',
+        help='whether to use Tornado\'s http server and wsgi container for external access'
+    )
     args = p.parse_args()
 
     settings = {
@@ -17,23 +21,27 @@ if __name__ == '__main__':
             'run_elements': {  # for demultiplexing reports, etc
                 'url': 'run_elements',
                 'item_title': 'element',
+                'id_field': 'id',
 
                 'schema': {
-                    'run_id':               {'type':  'string'},
-                    'lane':                 {'type': 'integer'},
-                    'barcode':              {'type':  'string'},
-                    'project':              {'type':  'string'},
+                    'id':                   {'type': 'string', 'required': True, 'unique': True},
+
+                    'run_id':               {'type':  'string', 'required': True},
+                    'lane':                 {'type': 'integer', 'required': True},
+                    'barcode':              {'type':  'string', 'required': True},
+                    'project':              {'type':  'string', 'required': True},
 
                     # for now, treat sample_id and library_id as 1:1 equivalent
-                    'library_id':           {'type':  'string'},
-                    'sample_id':            {'type':  'string'},
+                    'library_id':           {'type':  'string', 'required': True},
+                    'sample_id':            {'type':  'string', 'required': True},
 
-                    'pc_pass_filter':       {'type':   'float'},
-                    'passing_filter_reads': {'type': 'integer'},
-                    'yield_in_gb':          {'type':   'float'},
+                    'pc_pass_filter':       {'type':   'float', 'required': True},
+                    'passing_filter_reads': {'type': 'integer', 'required': True},
+                    'pc_reads_in_lane':     {'type':   'float', 'required': True},
+                    'yield_in_gb':          {'type':   'float', 'required': True},
 
-                    'pc_q30_r1':            {'type':   'float'},
-                    'pc_q30_r2':            {'type':   'float'}
+                    'pc_q30_r1':            {'type':   'float', 'required': True},
+                    'pc_q30_r2':            {'type':   'float', 'required': True}
 
                 }
 
@@ -41,38 +49,43 @@ if __name__ == '__main__':
 
             'unexpected_barcodes': {
                 'url': 'unexpected_barcodes',
+                'item_title': 'unexpected_barcode',
+                'id_field': 'id',
 
                 'schema': {
-                    'run_id':               {'type':  'string'},
-                    'lane':                 {'type': 'integer'},
-                    'barcode':              {'type':  'string'},
-                    'passing_filter_reads': {'type':   'float'},
-                    'pc_reads_in_lane':     {'type':   'float'}
+                    'id':                   {'type': 'string', 'required': True, 'unique': True},
+
+                    'run_id':               {'type':  'string', 'required': True},
+                    'lane':                 {'type': 'integer', 'required': True},
+                    'barcode':              {'type':  'string', 'required': True},
+                    'passing_filter_reads': {'type':   'float', 'required': True},
+                    'pc_reads_in_lane':     {'type':   'float', 'required': True}
                 }
             },
 
             'samples': {
                 'url': 'samples',
+                'id_field': 'library_id',
 
                 'schema': {
-                    'project':                  {'type': 'string'},
-                    'library_id':               {'type': 'string'},
-                    'sample_id':                {'type': 'string'},
-                    'user_sample_id':           {'type': 'string'},
+                    'project':                  {'type': 'string', 'required': True},
+                    'library_id':               {'type': 'string', 'required': True, 'unique': True},
+                    'sample_id':                {'type': 'string', 'required': True},
+                    'user_sample_id':           {'type': 'string', 'required': True},
 
-                    'yield_in_gb':              {'type':   'float'},
-                    'initial_reads':            {'type': 'integer'},  # used to be 'no adaptor reads'
-                    'passing_filter_reads':     {'type': 'integer'},
-                    'nb_mapped_reads':          {'type': 'integer'},
-                    'pc_mapped_reads':          {'type':   'float'},
-                    'nb_properly_mapped_reads': {'type': 'integer'},
-                    'pc_properly_mapped_reads': {'type':   'float'},
-                    'nb_duplicate_reads':       {'type': 'integer'},
-                    'pc_duplicate_reads':       {'type':   'float'},
-                    'median_coverage':          {'type':   'float'},
-                    'pc_callable':              {'type':   'float'},
-                    'pc_q30_r1':                {'type':   'float'},
-                    'pc_q30_r2':                {'type':   'float'}
+                    'yield_in_gb':              {'type':   'float', 'required': True},
+                    'initial_reads':            {'type': 'integer', 'required': True},  # used to be 'no adaptor reads'
+                    'passing_filter_reads':     {'type': 'integer', 'required': True},
+                    'nb_mapped_reads':          {'type': 'integer', 'required': True},
+                    'pc_mapped_reads':          {'type':   'float', 'required': True},
+                    'nb_properly_mapped_reads': {'type': 'integer', 'required': True},
+                    'pc_properly_mapped_reads': {'type':   'float', 'required': True},
+                    'nb_duplicate_reads':       {'type': 'integer', 'required': True},
+                    'pc_duplicate_reads':       {'type':   'float', 'required': True},
+                    'median_coverage':          {'type':   'float', 'required': True},
+                    'pc_callable':              {'type':   'float', 'required': True},
+                    'pc_q30_r1':                {'type':   'float', 'required': True},
+                    'pc_q30_r2':                {'type':   'float', 'required': True}
                 }
             },
 
@@ -107,12 +120,6 @@ if __name__ == '__main__':
         'ITEM_METHODS': ['GET', 'PUT', 'DELETE']
     }
 
-    for k, v in settings['DOMAIN'].items():
-        schema = v.get('schema')
-        if schema:
-            for field, config in schema.items():
-                config['required'] = True
-
     app = eve.Eve(settings=settings)
 
     """
@@ -125,8 +132,14 @@ if __name__ == '__main__':
     http://host:port/things?where={%22sample_project%22:%22this%22}
     """
 
-    args = p.parse_args()
-    app.run(port=4999)
-    #http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
-    #http_server.listen(args.port)
-    #tornado.ioloop.IOLoop.instance().start()
+    if args.tornado:
+        import tornado.wsgi
+        import tornado.httpserver
+        import tornado.ioloop
+
+        http_server = tornado.httpserver.HTTPServer(tornado.wsgi.WSGIContainer(app))
+        http_server.listen(args.port)
+        tornado.ioloop.IOLoop.instance().start()
+
+    else:
+        app.run('localhost', 4999, debug=args.debug)
