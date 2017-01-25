447c636c5803d598d38fcc23838ad062d5d5e01d
tcezard <timothee.cezard@ed.ac.uk>
2017-01-25 15:25:19 +0000

    Store more information in the sample and aggregate per container afterward. Simplify the functions that give status per plate/project sample_status endpoint now has a detailed option for statuses that retrieve all processes instead of only the relevant process

--- rest_api/limsdb/queries/sample_status.py
+++ rest_api/limsdb/queries/sample_status.py
@@ -10,11 +10,12 @@ from rest_api.limsdb.queries import get_samples_and_processes, non_QC_queues, ge
 from config import project_status as status_cfg
 
 
-
 class Sample:
     def __init__(self):
-        self.sample_name=None
-        self.project_name=None
+        self.sample_name = None
+        self.project_name = None
+        self.plate_name = None
+        self.original_name = None
         self.completed_processes = []
         self._processes = set()
         self.queue_location = {}
@@ -32,7 +33,6 @@ class Sample:
     @property
     def processes(self):
         return sorted(self._processes, key=operator.itemgetter(1), reverse=True)
-
 
     def _get_all_status_and_date(self):
         '''
@@ -54,20 +54,19 @@ class Sample:
                 status_order.append(status)
                 process_per_status.append([process])
 
-
         if not self._all_statuses_and_date:
             self._all_statuses_and_date = []
             processes_per_status = []
             status_order = []
             current_status = status = status_cfg.status_order[0]
             for process, date, process_type, process_id in sorted(self._processes, key=operator.itemgetter(1)):
-                process_dict = {'name':process, 'date':date.strftime('%b %d %Y'), 'type':process_type, 'process_id': process_id}
-                #This part find the new status
+                process_dict = {'name':process, 'date':date.strftime('%b %d %Y'), 'type':process_type,
+                                'process_id': process_id}
+                # This part find the new status
                 if process_type == 'complete' and process in status_cfg.step_completed_to_status:
                     status = status_cfg.step_completed_to_status.get(process)
                 elif process_type == 'queued' and process in status_cfg.step_queued_to_status:
                     status = status_cfg.step_queued_to_status.get(process)
-
 
                 if process_type == 'complete':
                     add_process_with_status(status_order, processes_per_status, current_status, process_dict)
@@ -97,11 +96,10 @@ class Sample:
                 if not status:
                     status = new_status
                     self._status_and_date = (status, date)
-                elif status==new_status:
+                elif status == new_status:
                     self._status_and_date = (status, date)
                 else:
                     break
-
         return self._status_and_date
 
     @property
@@ -137,48 +135,47 @@ class Sample:
             if process_type == 'complete':
                 return date
 
+    @property
+    def finished_date(self):
+        if self.status == status_cfg.status_names['FINISHED']:
+            return self.status_date
+        return None
+
     def all_status(self):
         return self._get_all_status_and_date()
-
 
     def to_json(self):
         return {
             'sample_id': self.sample_name,
             'project_id': self.project_name,
-            'status': self.all_status()
+            'statuses': self.all_status(),
+            'current_status': self.status,
+            'started_date': self.started_date.isoformat() + 'Z',
+            'finished_date': self.finished_date.isoformat() + 'Z'
         }
-
 
 
 class Container:
     def __init__(self):
-        self.samples = defaultdict(Sample)
+        self.samples = []
         self.project_id = None
         self.name = None
 
     def samples_per_status(self):
         sample_per_status = defaultdict(list)
-        for sample_name in self.samples:
-            sample_per_status[self.samples[sample_name].status].append(sample_name)
-            for status in self.samples[sample_name].additional_status:
-                sample_per_status[status].append(sample_name)
+        for sample in self.samples:
+            sample_per_status[sample.status].append(sample.sample_name)
+            for status in sample.additional_status:
+                sample_per_status[status].append(sample.sample_name)
         return sample_per_status
 
     @property
     def library_types(self):
-        return ', '.join(set([
-                            self.samples[sample_name].library_type
-                            for sample_name in self.samples
-                            if self.samples[sample_name].library_type
-                            ]))
+        return ', '.join(set([ sample.library_type for sample in self.samples if sample.library_type ]))
 
     @property
     def species(self):
-        return ', '.join(set([
-                                 self.samples[sample_name].species
-                                 for sample_name in self.samples
-                                 if self.samples[sample_name].species
-                                 ]))
+        return ', '.join(set([sample.species for sample in self.samples if sample.species ]))
 
     def to_json(self):
         ret = {
@@ -208,8 +205,8 @@ class Project(Container):
             'open_date': self.open_date,
             'researcher_name': self.researcher_name,
             'nb_quoted_samples': self.nb_quoted_samples,
-            'finished_date': self.finished_date,
-            'started_date': self.started_date
+            'finished_date': self.finished_date.isoformat() + 'Z',
+            'started_date': self.started_date.isoformat() + 'Z'
         }
         ret.update(self.samples_per_status())
 
@@ -217,130 +214,96 @@ class Project(Container):
 
     @property
     def finished_date(self):
-        finished_samples = [
-                (sample_name, self.samples[sample_name].status_date)
-                for sample_name in self.samples
-                if self.samples[sample_name].status == status_cfg.status_names['FINISHED']
+        finished_dates = [
+                sample.status_date
+                for sample in self.samples
+                if sample.status == status_cfg.status_names['FINISHED']
             ]
-        if finished_samples and self.nb_quoted_samples and len(finished_samples) >= int(self.nb_quoted_samples):
-            finished_samples.sort(key=operator.itemgetter(1))
-            s, d = finished_samples[-1]
-            return d.isoformat() + 'Z'
+        if finished_dates and self.nb_quoted_samples and len(finished_dates) >= int(self.nb_quoted_samples):
+            finished_dates.sort()
+            return finished_dates[-1]
         return None
 
     @property
     def started_date(self):
         if self.samples:
-            dates = sorted([
-                              self.samples.get(sample_name).started_date
-                              for sample_name in self.samples
-                              if self.samples.get(sample_name).started_date
-                           ])
-            if dates:
-                return dates[0]
+            started_dates = sorted([ sample.started_date for sample in self.samples  if sample.started_date ])
+            if started_dates:
+                return started_dates[0]
         return None
 
+def _create_samples(session):
+    """This function queries the lims database for sample information and create Sample objects"""
+    start_time = datetime.now()
+    match = json.loads(request.args.get('match', '{}'))
+    all_samples = defaultdict(Sample)
+    project_id = match.get('project_id')
+    sample_id = match.get('sample_id')
+    detailed = bool(request.args.get('detailed', False))
+    if detailed:
+        list_process = None
+    else:
+        list_process = list(status_cfg.step_completed_to_status) \
+                       + list(status_cfg.additional_step_completed) \
+                       + list(status_cfg.library_type_step_completed)
+
+    for result in get_sample_info(session, project_id, sample_id, udfs=['Prep Workflow', 'Species']):
+        (pjct_name, sample_name, container, wellx, welly, udf_name, udf_value) = result
+        s = all_samples[sanitize_user_id(sample_name)]
+        s.sample_name = sanitize_user_id(sample_name)
+        s.project_name = pjct_name
+        s.plate_name = container
+        s.original_name = sample_name
+
+        if udf_name == 'Prep Workflow':
+            all_samples[sanitize_user_id(sample_name)].planned_library = udf_value
+        if udf_name == 'Species':
+            all_samples[sanitize_user_id(sample_name)].species = udf_value
+
+    for result in get_samples_and_processes(session,  project_id, sample_id,
+                                            workstatus='COMPLETE', list_process=list_process):
+        (pjct_name, sample_name, process_name, process_status, date_run, process_id) = result
+        all_samples[sanitize_user_id(sample_name)].add_completed_process(process_name, date_run, process_id)
+
+
+    for result in non_QC_queues(session, project_id, sample_id, list_process=status_cfg.step_queued_to_status):
+        pjct_name, sample_name, process_name, queued_date, queue_id = result
+        all_samples[sanitize_user_id(sample_name)].add_queue_location(process_name, queued_date, queue_id)
+
+    return all_samples.values()
+
+def sample_status(session):
+    """This function queries the lims database for sample information and return json representation"""
+    samples = _create_samples(session)
+    return [s.to_json() for s in samples]
 
 
 def sample_status_per_project(session):
     """This function queries the lims database for sample information and aggregate at the project name level"""
+    samples = _create_samples(session)
     match = json.loads(request.args.get('match', '{}'))
     project_name = match.get('project_id')
     all_projects = defaultdict(Project)
     for project_info in get_project_info(session, project_name, udfs=['Number of Quoted Samples']):
-        pjct_name, open_date, firstname, lastname, udf_name , nb_quoted_samples = project_info
+        pjct_name, open_date, firstname, lastname, udf_name, nb_quoted_samples = project_info
         all_projects[pjct_name].name = pjct_name
         all_projects[pjct_name].open_date = open_date.isoformat() + 'Z'
         all_projects[pjct_name].researcher_name = '%s %s' % (firstname, lastname)
         all_projects[pjct_name].nb_quoted_samples = nb_quoted_samples
 
-    for result in get_samples_and_processes(
-            session,
-            project_name,
-            workstatus='COMPLETE',
-            list_process=list(status_cfg.step_completed_to_status) \
-                       + list(status_cfg.additional_step_completed) \
-                       + list(status_cfg.library_type_step_completed)
-    ):
-        (pjct_name, sample_name, process_name, process_status, date_run, process_id) = result
-        all_projects[pjct_name].samples[sanitize_user_id(sample_name)].add_completed_process(process_name, date_run)
-
-    for result in non_QC_queues(session, project_name, list_process=status_cfg.step_queued_to_status):
-        pjct_name, sample_name, process_name, queued_date, queue_id = result
-        all_projects[pjct_name].samples[sanitize_user_id(sample_name)].add_queue_location(process_name, queued_date)
-
-    for result in get_sample_info(session, project_name, udfs=['Prep Workflow', 'Species']):
-        (pjct_name, sample_name, container, wellx, welly, udf_name, udf_value) = result
-        if udf_name == 'Prep Workflow':
-            all_projects[pjct_name].samples[sanitize_user_id(sample_name)].planned_library = udf_value
-        if udf_name == 'Species':
-            all_projects[pjct_name].samples[sanitize_user_id(sample_name)].species = udf_value
+    for sample in samples:
+        all_projects[sample.project_name].samples.append(sample)
 
     return [p.to_json() for p in all_projects.values()]
 
 
 def sample_status_per_plate(session):
     """This function queries the lims database for sample information and aggregate at the plate name level"""
-    match = json.loads(request.args.get('match', '{}'))
-    project_name = match.get('project_id')
+    samples = _create_samples(session)
     all_plates = defaultdict(Container)
-    sample_to_container = {}
-    for result in get_sample_info(session, project_name, udfs=['Prep Workflow', 'Species']):
-        (pjct_name, sample_name, container, wellx, welly, udf_name, udf_value) = result
-        all_plates[container].name = container
-        all_plates[container].project_id = pjct_name
-        if udf_name == 'Prep Workflow':
-            all_plates[container].samples[sanitize_user_id(sample_name)].planned_library = udf_value
-        if udf_name == 'Species':
-            all_plates[container].samples[sanitize_user_id(sample_name)].species = udf_value
-        sample_to_container[sample_name] = container
-
-    for result in get_samples_and_processes(
-            session,
-            project_name,
-            workstatus='COMPLETE',
-            list_process=list(status_cfg.step_completed_to_status) \
-                       + list(status_cfg.additional_step_completed) \
-                       + list(status_cfg.library_type_step_completed)
-    ):
-        (pjct_name, sample_name, process_name, process_status, date_run, process_id) = result
-        container = sample_to_container.get(sample_name)
-        all_plates[container].samples[sanitize_user_id(sample_name)].add_completed_process(process_name, date_run)
-
-    for result in non_QC_queues(session, project_name, list_process=status_cfg.step_queued_to_status):
-        pjct_name, sample_name, process_name, queued_date, queue_id = result
-        container = sample_to_container.get(sample_name)
-        all_plates[container].samples[sanitize_user_id(sample_name)].add_queue_location(process_name, queued_date)
+    for sample in samples:
+        all_plates[sample.plate_name].samples.append(sample)
+        all_plates[sample.plate_name].name = sample.plate_name
+        all_plates[sample.plate_name].project_id = sample.project_name
 
     return [p.to_json() for p in all_plates.values()]
-
-def sample_status(session):
-    match = json.loads(request.args.get('match', '{}'))
-    all_samples = defaultdict(Sample)
-    project_name = match.get('project_id')
-    sample_name = match.get('sample_id')
-
-    for result in get_sample_info(session, project_name, sample_name, udfs=['Prep Workflow', 'Species']):
-        (pjct_name, sample_name, container, wellx, welly, udf_name, udf_value) = result
-        all_samples[sanitize_user_id(sample_name)].sample_name = sample_name
-        all_samples[sanitize_user_id(sample_name)].project_name = pjct_name
-        if udf_name == 'Prep Workflow':
-            all_samples[sanitize_user_id(sample_name)].planned_library = udf_value
-        if udf_name == 'Species':
-            all_samples[sanitize_user_id(sample_name)].species = udf_value
-
-    for result in get_samples_and_processes(
-            session,
-            project_name,
-            sample_name,
-            workstatus='COMPLETE'
-    ):
-        (pjct_name, sample_name, process_name, process_status, date_run, process_id) = result
-        all_samples[sanitize_user_id(sample_name)].add_completed_process(process_name, date_run, process_id)
-
-        for result in non_QC_queues(session, project_name, sample_name):
-            pjct_name, sample_name, process_name, queued_date, queue_id = result
-            all_samples[sanitize_user_id(sample_name)].add_queue_location(process_name, queued_date, queue_id)
-
-    return [s.to_json() for s in all_samples.values()]
-
