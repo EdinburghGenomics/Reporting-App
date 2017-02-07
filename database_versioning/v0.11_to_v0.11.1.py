faa4bda4547955046b35df828787cf71f2be8124
tcezard <timothee.cezard@ed.ac.uk>
2017-02-07 11:50:08 +0000

    Update the script to also fill in data_deleted when the field does not exists

--- database_versioning/v0.11_to_v0.11.1.py
+++ database_versioning/v0.11_to_v0.11.1.py
@@ -4,6 +4,7 @@ required for v0.11.1 The changes required are:
   - Move deletion information contained in the analysis_driver_proc.status to sample.data_deleted
     analysis_driver_proc.status = deleted  ==> sample.data_deleted = all
   - reverting analysis_driver_proc.status = finished
+  - set sample.data_deleted = none when sample.data_deleted does not exists
 
 This script requires a NoSQL database and an instance of Reporting-App v0.11.1 to be running.
 """
@@ -11,6 +12,16 @@ This script requires a NoSQL database and an instance of Reporting-App v0.11.1 t
 
 import argparse
 from egcg_core import rest_communication
+
+
+def find_non_marked_samples(comm):
+    return comm.get_documents(
+        'samples',
+        quiet=True,
+        match={'data_deleted': None},
+        all_pages=True
+    )
+
 
 
 def find_deleted_samples(comm):
@@ -22,23 +33,21 @@ def find_deleted_samples(comm):
     )
 
 
-def update_data_deleted(comm, samples):
+def update_data_deleted(comm, samples, deletion_status):
     for sample in samples:
         comm.patch_entry(
             'samples',
-            {'data_deleted': 'all'},
+            {'data_deleted': deletion_status},
             'sample_id', sample.get('sample_id')
         )
 
 
 def update_most_recent_proc(comm, samples):
     for sample in samples:
-        patch_content = sample.get('most_recent_proc')
-        patch_content['status'] = 'finished'
-        element_id = patch_content.pop('proc_id')
+        element_id = sample.get('most_recent_proc').get('proc_id')
         comm.patch_entry(
             'analysis_driver_procs',
-            patch_content,
+            {'status': 'finished'},
             id_field='proc_id',
             element_id=element_id
         )
@@ -54,5 +63,10 @@ if __name__ == '__main__':
     comm = rest_communication.Communicator((args.username, args.password), args.baseurl)
 
     samples = find_deleted_samples(comm)
-    update_data_deleted(comm, samples)
+    update_data_deleted(comm, samples, deletion_status='all')
     update_most_recent_proc(comm, samples)
+
+    samples = find_non_marked_samples(comm)
+    update_data_deleted(comm, samples, deletion_status='none')
+
+
