56de61dae537aeb92629a9527e5d5a3cdfca359b
tcezard <timothee.cezard@ed.ac.uk>
2017-02-16 14:43:48 +0000

    Get all queued sample in detailed view + add Create CFP Batch

--- rest_api/limsdb/queries/__init__.py
+++ rest_api/limsdb/queries/__init__.py
@@ -146,11 +146,8 @@ if __name__ == "__main__":
     from rest_api.limsdb import get_session
     from datetime import datetime, timedelta
     session = get_session()
-    now = datetime.now()
-    threshold = now - timedelta(7)
 
-    # res = runs_info(session, run_ids=['170117_E00328_0173_BHCKWYALXX', '170206_E00306_0218_BHCLJJALXX'])
-    res = runs_info(session, run_status=['RunStarted'])
+    res = non_QC_queues(session, sample_name='X15060P005A05')
 
     # res = runs_info(session, time_since=threshold)
 
