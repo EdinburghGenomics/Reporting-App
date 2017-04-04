98eb03692da966b761cf52623dd0b4b9e70d9103
Murray Wham <murray.wham@ed.ac.uk>
2017-04-04 14:55:56 +0100

    #93: Splitting timeline off into stage_timeline.js, updating vis to 4.19.1. Adding tooltips and exit status colouring.

---
+++ reporting_app/static/stage_timeline.js
@@ -0,0 +1,57 @@
+
+function _to_moment(str_date) {
+    return moment(str_date, 'DD_MM_YYYY_HH:mm:SS');
+}
+
+
+function _reformat(date) {
+    return _to_moment(date).format('YYYY-MM-DD HH:mm:ss');
+}
+
+
+function build_tooltip(stage) {
+    var t = '<div><b>' + stage.stage_name + '</b><br/>Started: ' + _reformat(stage.date_started) + '<br/>';
+    if (stage.date_finished) {
+        t += 'Finished: ' + _reformat(stage.date_finished) + '<br/>';
+        t += 'Exit status: ' + stage.exit_status + '<br/>';
+    }
+    t += '</div>';
+    return t;
+}
+
+
+function stage_timeline(div_id, proc) {
+    var stages = proc.stages.map(
+        function(s) {
+            var o = {
+                'start': _to_moment(s.date_started),
+                'style': 'display: block; overflow: visible',
+                'content': s.stage_name,
+                'title': build_tooltip(s)
+            };
+
+            if (s.date_finished) {
+                o['end'] = _to_moment(s.date_finished);
+                if (s.exit_status == 0) {
+                    o['style'] += '; background-color: #c2e0c6; border-color: #0e8a16';  // green
+                } else {
+                    o['style'] += '; background-color: #f7c6c7; border-color: #b60205';  // red
+                }
+            } else {
+                o['end'] = moment();
+            }
+            return o;
+        }
+    );
+
+    var items = new vis.DataSet(stages);
+    var startDate = _to_moment(proc._created);
+    var options = {
+            'tooltip': {
+            'followMouse': true,
+            'overflowMethod': 'cap'
+        }
+    };
+
+    var timeline = new vis.Timeline(document.getElementById(div_id), items, options);
+}
