04461ef0bf7dbdbf4320e539879238f83425a454
Murray Wham <murray.wham@ed.ac.uk>
2017-04-11 13:21:49 +0100

    #93: Passing proc through tojson, checking for proc.stages == undefined, reinstating timeline collapsibility. Adding time elapsed to tooltip.

--- reporting_app/static/stage_timeline.js
+++ reporting_app/static/stage_timeline.js
@@ -11,47 +11,54 @@ function _reformat(date) {
 
 function build_tooltip(stage) {
     var t = '<div><b>' + stage.stage_name + '</b><br/>Started: ' + _reformat(stage.date_started) + '<br/>';
+
+    var stage_end;
     if (stage.date_finished) {
+        stage_end = _to_moment(stage.date_finished);
         t += 'Finished: ' + _reformat(stage.date_finished) + '<br/>';
         t += 'Exit status: ' + stage.exit_status + '<br/>';
+    } else {
+        stage_end = moment();
     }
-    t += '</div>';
+    t += 'Time elapsed: ' + moment.duration(stage_end.diff(_to_moment(stage.date_started))).humanize();
+    t += '<br/></div>';
     return t;
 }
 
 
 function stage_timeline(div_id, proc) {
-    var stages = proc.stages.map(
-        function(s) {
-            var o = {
-                'start': _to_moment(s.date_started),
-                'style': 'display: block; overflow: visible',
-                'content': s.stage_name,
-                'title': build_tooltip(s)
-            };
+    var stages;
 
-            if (s.date_finished) {
-                o['end'] = _to_moment(s.date_finished);
-                if (s.exit_status == 0) {
-                    o['style'] += '; background-color: #c2e0c6; border-color: #0e8a16';  // green
+    if (proc.stages === undefined) {
+        stages = []
+    } else {
+        stages = proc.stages.map(
+            function(s) {
+                var o = {
+                    'start': _to_moment(s.date_started),
+                    'style': 'display: block; overflow: visible',
+                    'content': s.stage_name,
+                    'title': build_tooltip(s)
+                };
+
+                if (s.date_finished) {
+                    o['end'] = _to_moment(s.date_finished);
+                    if (s.exit_status == 0) {
+                        o['style'] += '; background-color: #c2e0c6; border-color: #0e8a16';  // green
+                    } else {
+                        o['style'] += '; background-color: #f7c6c7; border-color: #b60205';  // red
+                    }
                 } else {
-                    o['style'] += '; background-color: #f7c6c7; border-color: #b60205';  // red
+                    o['end'] = moment();
                 }
-            } else {
-                o['end'] = moment();
+                return o;
             }
-            return o;
-        }
-    );
+        );
+    }
 
     var items = new vis.DataSet(stages);
     var startDate = _to_moment(proc._created);
-    var options = {
-            'tooltip': {
-            'followMouse': true,
-            'overflowMethod': 'cap'
-        }
-    };
+    var options = {'tooltip': {'followMouse': true, 'overflowMethod': 'cap'}};
 
     var timeline = new vis.Timeline(document.getElementById(div_id), items, options);
 }
