6d5d81b30cebc4a105ce7e6b37446a1b5c8e8715
tcezard <timothee.cezard@ed.ac.uk>
2019-07-09 14:22:10 +0100

    fix the loading of data to the LIMS

--- docker/reporting_app.sh
+++ docker/reporting_app.sh
@@ -16,6 +16,9 @@ mkdir -p /opt/logs/
 
 /opt/database/mongodb-linux-x86_64-rhel70-3.4.1/bin/mongod > /opt/database/mongod.log &
 REPORTINGCONFIG=/opt/reporting.yaml /opt/python/bin/python bin/run_app.py rest_api
-if [ -e /opt/data_for_clarity_lims.yaml ] then
-    REPORTINGCONFIG=/opt/reporting.yaml PYTHONPATH=/opt/python/bin/python docker/load_data_to_lims_db.py --yaml_file /opt/data_for_clarity_lims.yaml
+
+# Load the data to the LIMS if it has been provided
+if [ -e /opt/data_for_clarity_lims.yaml ]
+then
+    REPORTINGCONFIG=/opt/reporting.yaml PYTHONPATH=. /opt/python/bin/python docker/load_data_to_lims_db.py --yaml_file /opt/data_for_clarity_lims.yaml
 fi
